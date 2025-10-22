20739c1093081c381b62cad7577f6ac4839510b9
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-10-22 08:36:27 -0500

    new app

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles1.css';
+const Navbar = () => {
+
+  return(
+  <nav>
+    <ul>
+      <li>Hello</li>  
+      <li>Item </li>
+        
+    </ul>
+   </nav>
+  )
+}
+
+export default Navbar;  
